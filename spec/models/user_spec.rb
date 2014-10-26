2b7c74b9fdd747ed8e59d81b0eeb5185ffce2c6e
Ioannis-Andreas Philippas <iphilippas@gmail.com>
2014-10-26 21:40:54 +0200

    Add user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,24 @@ describe User do
 	it { should respond_to(:email) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:auth_token) }
+	
 	it { should validate_presence_of(:email) }
 
 	it { should validate_uniqueness_of(:email) }
 	it { should validate_confirmation_of(:password) }
 	it { should allow_value('example@domain.com').for(:email) }
-	
+	it { should validate_uniqueness_of(:auth_token)}
+		
 	it { should be_valid }
+
+	describe "#generate_authentication_token!" do
+		it "generates an unique token" do
+			Devise.stub(:friendly_token).and_return("uniquetoken12345")
+			@user.generate_authentication_token!
+			expect(@user.auth_token).to eql "uniquetoken12345"
+		end
+	end
 end
 
 
